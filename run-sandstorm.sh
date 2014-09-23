3c08efa3d6c249137efaee4a62bf93a43a9a5c66
Jason Paryani <github@jparyani.com>
2014-09-23 16:49:01 -0700

    Add sandstorm run script and pkgdef

---
+++ run-sandstorm.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -ex
+
+export HOME=/var
+export LANG=en_US.UTF-8
+
+cd /var
+
+if ! test -e mail; then
+  mkdir -p mail/cur
+  mkdir -p mail/new
+  mkdir -p mail/tmp
+
+  chmod -R 770 mail
+
+  mkdir -p /var/.local/share/Mailpile
+  cp -r /default /var/.local/share/Mailpile
+  cp -r /.gnupg /var
+
+  # /usr/bin/python /mp --setup
+  # /usr/bin/python /mp --set sys.http_host=0.0.0.0
+  # /usr/bin/python /mp --add /var/mail/
+  # /usr/bin/python /mp --set prefs.rescan_interval=5
+  # /usr/bin/python /mp --set prefs.empty_outbox_interval=5
+fi
+
+/usr/bin/python /mp --rescan all
+/usr/bin/python /mp --www= --wait
